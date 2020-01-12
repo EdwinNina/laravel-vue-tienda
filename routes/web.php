11044cd38b9c390e1b8438e8c2c0104a426f454e
EdwinSys <edrnina05@gmail.com>
2020-01-12 18:57:11 -0400

    Finalizacion de la creacion de la tabla de categorias y tambien se realizo la creacion del controlador, modelo y migracion de la tabla de productos

--- routes/web.php
+++ routes/web.php
@@ -12,9 +12,17 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('contenido.contenido');
 });
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+//rutas de la categoria
+Route::get('/categoria','CategoryController@index');
+Route::post('/categoria/agregar','CategoryController@store');
+Route::put('/categoria/actualizar','CategoryController@update');
+Route::put('/categoria/activar','CategoryController@activar');
+Route::put('/categoria/desactivar','CategoryController@desactivar');
+
